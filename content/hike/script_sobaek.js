a3e4bbcc94a06682f020c99a02cb3fd406ab184b
ekxk <97841655+ekxk@users.noreply.github.com>
2023-08-19 10:32:16 +0900

    Update script_sobaek.js

--- content/hike/script_sobaek.js
+++ content/hike/script_sobaek.js
@@ -3,7 +3,7 @@ var currentImage = document.getElementById("current-image");
 
 for (var i = 1; i <= 16; i++) {
   var thumb = document.createElement("img");
-  thumb.src = "sobaek" + i + ".jpg";
+  thumb.src = "image" + i + ".jpg";
   thumb.alt = "Image " + i;
   thumb.classList.add("thumb");
   imageThumbs.appendChild(thumb);
@@ -12,4 +12,15 @@ for (var i = 1; i <= 16; i++) {
       currentImage.src = this.src;
     }
   );
+}
+
+function changeImage(current) {
+  var imagesNumber = 7;
+  for (i=1; i<=imagesNumber; i++) {
+    if (i == current) {
+        document.getElementById("imag" + current).style.display = "block";
+    } else {
+        document.getElementById("imag" + i).style.display = "none";
+    }
+}
 }
